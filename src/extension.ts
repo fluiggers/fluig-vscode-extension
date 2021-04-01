224c098d52ba72178970b1a43be18fbcea5f0bb2
Bruno Gasparetto <bruno.gasparetto@ms.sebrae.com.br>
2021-04-01 11:24:22 -0400

    Adição de Doctype e definição de charset ao criar formulário

--- src/extension.ts
+++ src/extension.ts
@@ -307,8 +307,11 @@ function onMobileSync(user) {
  * @returns {string}
  */
  function createFormContent(): string {
-    return `<html>
+    return `<!DOCTYPE html>
+<html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style-guide/css/fluig-style-guide.min.css"/>
     <script type="text/javascript" src="/portal/resources/js/jquery/jquery.js"></script>
     <script type="text/javascript" src="/portal/resources/js/jquery/jquery-ui.min.js"></script>
