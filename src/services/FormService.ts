b345fd4ea549151f30208fed6acdd30da21cf3d0
Pedro Costa <pedro.costa.neto@outlook.com.br>
2021-06-18 08:43:22 -0300

    Baixar os arquivos do formulario

--- src/services/FormService.ts
+++ src/services/FormService.ts
@@ -3,6 +3,7 @@ import * as soap from 'soap';
 import { window, workspace } from "vscode";
 import { ServerService } from "./ServerService";
 import { DocumentDTO } from "../models/DocumentDTO";
+import { posix } from "path";
 
 export class FormService {
     private static URL_WSDL_CARD_INDEX_SERVICE: string = "/webdesk/ECMCardIndexService?wsdl";
@@ -48,7 +49,7 @@ export class FormService {
     /**
      * Retorna uma lista com o nome dos arquivos referente ao documento
      */
-     public static async getNameFiles(server: ServerDTO, documentId: Number): Promise<DocumentDTO[]> {
+     public static async getFileNames(server: ServerDTO, documentId: Number): Promise<string[]> {
         const uri = (server.ssl ? "https://" : "http://")
             + server.host
             + ":" + server.port
@@ -82,6 +83,47 @@ export class FormService {
         });
 
         return forms.result.item;
+    }
+
+    /**
+     * Retorna o base64 referente ao arquivo
+     */
+     public static async getFileBase64(server: ServerDTO, documentId: number, version: number, fileName: string, ) {
+        const uri = (server.ssl ? "https://" : "http://")
+            + server.host
+            + ":" + server.port
+            + FormService.URL_WSDL_CARD_INDEX_SERVICE
+        ;
+
+        const params = {
+            username: server.username,
+            password: server.password,
+            companyId: server.companyId,
+            documentId: documentId,
+            colleagueId: server.username,
+            version: version,
+            nomeArquivo: fileName
+        };
+
+        const file: any = await new Promise((accept, reject) => {
+            soap.createClient(uri, (err: any, client: soap.Client) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                client.getCardIndexContent(params, (err: any, response: any) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+
+                    accept(response);
+                });
+            });
+        });
+
+        return file.folder;
     }
 
     /**
@@ -131,6 +173,33 @@ export class FormService {
         }
 
         const documentId = form.documentId;
-        const nameFiles = FormService.getNameFiles(server, documentId);
+        const version = form.version;
+        const folderName = form.documentDescription;
+
+        const fileNames = await FormService.getFileNames(server, documentId);
+
+        for(let fileName of fileNames) {
+            const base64 = await FormService.getFileBase64(server, documentId, version, fileName);
+            const fileContent = Buffer.from(base64, 'base64').toString('utf-8');
+            FormService.saveFile(folderName, fileName, fileContent);
+        }
+    }
+
+    /**
+     * Criar arquivo do formulario
+     */
+     public static saveFile(folder: string, name: string, content: string) {
+        if (!workspace.workspaceFolders) {
+            window.showInformationMessage("Você precisa estar em um diretório / workspace.");
+            return;
+        }
+
+        const workspaceFolderUri = workspace.workspaceFolders[0].uri;
+        const datasetUri = workspaceFolderUri.with({ path: posix.join(workspaceFolderUri.path, "forms", folder, name) });
+
+        workspace.fs.writeFile(
+            datasetUri,
+            Buffer.from(content, "utf-8")
+        );
     }
 }
